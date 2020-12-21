531856efb6c5ec2310ebee1d2e795d00c4fa25c7
Eric Chen <eric7chen@yahoo.com>
2020-12-21 17:49:00 -0600

    basic class with conversion dictionary

---
+++ convert.py
@@ -0,0 +1,11 @@
+import os
+
+class Convert():
+    factor_dictionary = {
+        "Kilometers": 1000,
+        "Meters": 1,
+        "Centimeters": 0.01,
+        "Millimeters": 0.001,
+        "Micrometers": 0.000001,
+        "Nanometers": 0.000000001
+    }
